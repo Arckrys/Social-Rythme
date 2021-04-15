421207a06b40420333751c7927fd2f04808187ca
Edzilo <Edzilo@yahoo.fr>
2021-04-15 18:05:52 +0200

    Note destroyed after finishline

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -7,27 +7,32 @@ public class Note : MonoBehaviour
 
     private Vector3 m_direction;
 
+    //A renseigner depuis l'éditeur
     [SerializeField] private float m_speed;
-    // Start is called before the first frame update
+
     void Start()
     {
+        //Ici on récupére la direction vers laquelle la note va se déplacer
         m_direction = transform.parent.transform.forward;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //La note se déplace dans la direction indiquée avec la vitesse renseignée
         transform.position += m_direction * Time.deltaTime * m_speed;
     }
 
+    //C'est dans cette zone que le joueur peut appuyer pour valider la note
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Note entered finish line");
     }
 
+    //La joueur n'a pas appuyé sur la note à temps, elle disparaît 
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("Note left finish line");
+        Destroy(gameObject);
 
     }
 }
